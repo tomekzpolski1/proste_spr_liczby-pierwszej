fb4f8912c3f151fa03c5a1968ecc8a0b54e917f5
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-04-01 08:02:33 +0200

    Add project files.

---
+++ ConsoleApp45/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sprawdzenie czy liczba jest pierwsza");
+            Console.WriteLine("podaj liczbę");
+            int user_number = int.Parse(Console.ReadLine());
+
+            for (int k = 2; k < user_number; k++)
+            {
+                if (user_number % k == 0)
+                {
+                    Console.WriteLine($"{user_number} Nie jest to liczba pierwsza");
+                    break;
+                }
+                //break;
+                else
+                {
+                    Console.WriteLine($"{user_number} To jest liczba pierwsza");
+                    break;
+                }
+            }
+
+        }
+    }
+}
